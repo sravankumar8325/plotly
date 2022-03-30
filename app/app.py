d0ff02160d249e35ea2e5a44071453da16decad8
root <root@ip-172-31-26-130.ec2.internal>
2022-03-30 16:50:25 +0000

    Initial version

---
+++ app/app.py
@@ -0,0 +1,39 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import flask
+
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config.suppress_callback_exceptions = True
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash 2020"),
+        html.Div(children="""Dash: A web application framework for Python."""),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
+                    {
+                        "x": [1, 2, 3],
+                        "y": [2, 4, 5],
+                        "type": "bar",
+                        "name": u"Montréal",
+                    },
+                ],
+                "layout": {"title": "Dash Data Visualization"},
+            },
+        ),
+    ]
+)
+
+
+if __name__ == "__main__":
+    import os
+
+    debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
+    app.run_server(host="0.0.0.0", port=8443, debug=debug)
